6021fbeabf578c5c438d4ffdd625e798c5998bcc
raztreuzz <raztreuzz1234@gmail.com>
2026-05-09 22:30:23 -0600

    style: add Punto Local theme palette

--- app/src/main/java/com/example/proyecto_final_seminario2/ui/theme/Color.kt
+++ app/src/main/java/com/example/proyecto_final_seminario2/ui/theme/Color.kt
@@ -2,10 +2,18 @@ package com.example.proyecto_final_seminario2.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val LocalPrimary = Color(0xFF00527F)
+val LocalPrimaryDark = Color(0xFF004471)
+val LocalPrimarySoft = Color(0xFFEAF5FB)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val LocalAccent = Color(0xFF0EA5A3)
+val LocalAccentSoft = Color(0xFFE5F7F6)
+
+val LocalBackground = Color(0xFFF8F9FE)
+val LocalSurface = Color(0xFFFFFFFF)
+val LocalTextPrimary = Color(0xFF202124)
+val LocalTextMuted = Color(0xFF667085)
+val LocalBorder = Color(0xFFE3E8EF)
+
+val LocalWarning = Color(0xFFF59E0B)
+val LocalDanger = Color(0xFFD92D20)
